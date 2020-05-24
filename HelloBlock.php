2c4d33324d20a84c4e2d48f1d0963d6b32f15557
earas24 <57501373+earas24@users.noreply.github.com>
2020-05-24 21:14:48 +0200

    Create HelloBlock.php

---
+++ HelloBlock.php
@@ -0,0 +1,62 @@
+<?php
+namespace Drupal\hello_world\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ * )
+ */
+class HelloBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
+
+    if (!empty($config['hello_block_settings'])) {
+      $name = $config['hello_block_settings'];
+    }
+    else {
+      $name = $this->t('to no one');
+    }
+    return array(
+      '#markup' => $this->t('Hello @name!', array(
+          '@name' => $name,
+        )
+      ),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $default_config = \Drupal::config('hello_world.settings');
+    $config = $this->getConfiguration();
+
+    $form['hello_block_settings'] = array (
+      '#type' => 'textfield',
+      '#title' => $this->t('Who'),
+      '#description' => $this->t('Who do you want to say hello to?'),
+      '#default_value' => isset($config['hello_block_settings']) ? $config['hello_block_settings'] : $default_config->get('hello.name'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('hello_block_settings', $form_state->getValue('hello_block_settings'));
+  }
+}
